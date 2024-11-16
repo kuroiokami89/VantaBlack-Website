3acc76b34f9200c714fa42f51d7a229f6f7c686d
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2024-11-16 02:14:31 +0100

    fixed services img paths

--- src/app/components/HeadComponent.js
+++ src/app/components/HeadComponent.js
@@ -7,7 +7,7 @@ export default function HeadComponent() {
     <Head>
       <title>Vantablack - Web Developer</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* If you really need to include an external stylesheet */}
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
     </Head>
