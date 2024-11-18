44723be108d6851a6b88652a7050a2a75e09599d
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2024-11-18 14:32:34 +0100

    Removed styled-components from Services

--- src/app/components/Services.js
+++ src/app/components/Services.js
@@ -1,126 +1,8 @@
 import { NeutralFace } from "./fonts";
-import styled from "styled-components";
-
-const StyledServices = styled.div`
-  margin-bottom: 200px;
-  display: flex;
-  flex-wrap: wrap;
-
-  /* Skill Styles */
-  .skill {
-    border: 1px solid white;
-    padding: 1.5%;
-    display: flex;
-    align-items: flex-end;
-    height: 450px;
-    width: 32.5%;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    position: relative;
-    overflow: hidden;
-    cursor: pointer;
-    transition: all 0.3s ease;
-  }
-
-  .skill::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.55);
-    z-index: 0;
-    transition: opacity 0.6s ease-in;
-  }
-
-  .skill:hover::before {
-    opacity: 0.55;
-  }
-
-  .skill h3 {
-    font-size: 2.5rem;
-    position: relative;
-    z-index: 1;
-  }
-
-  .skill:nth-child(1) {
-    width: 35%;
-    border-left: 0;
-    border-bottom: 0;
-  }
-
-  .skill:nth-child(2) {
-    width: 65%;
-    border-right: 0;
-  }
-
-  .skill:nth-child(3) {
-    width: 35%;
-    border-left: 0;
-  }
-
-  .skill:nth-child(5) {
-    border-right: 0;
-  }
-
-  /* Skill Titles */
-  .skill-title {
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: space-between;
-    cursor: unset;
-    border-bottom: 0;
-    border-right: 0;
-  }
-
-  /* Individual Skill Backgrounds */
-  .skill-1 {
-    background-image: url("./front-end.jpg");
-    border-bottom: 0;
-  }
-
-  .skill-2 {
-    background-image: url("./ux-ui.jpg");
-    border-right: 0;
-  }
-
-  .skill-3 {
-    background-image: url("./video-editing.jpg");
-    border-right: 0;
-  }
-
-  .skill-4 {
-    background-image: url("./graphic.jpeg");
-  }
-
-  /* Media Queries */
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-
-    .skill {
-      width: 100% !important;
-      height: 400px;
-      padding: 20px;
-      border-left: 0;
-      border-right: 0;
-      cursor: auto;
-    }
-
-    .skill::before {
-      opacity: 1 !important;
-    }
-
-    .skill:hover::before {
-      opacity: 1 !important;
-    }
-  }
-`;
 
 export default function Services() {
   return (
-    <StyledServices className={`${NeutralFace.className}`}>
+    <div id="services" className={`${NeutralFace.className}`}>
       <div className="skill skill-title">
         <h3>My skills</h3>
       </div>
@@ -136,6 +18,6 @@ export default function Services() {
       <div className="skill skill-4">
         <h3>Graphic & Motion Design</h3>
       </div>
-    </StyledServices>
+    </div>
   );
 }
