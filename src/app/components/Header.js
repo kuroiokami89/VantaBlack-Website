d6f2743c517ffc5767a18d67cb100fa8e57c338c
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2024-12-10 13:05:07 +0100

    Update CV and header links

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,13 +5,12 @@ export default function Header() {
   return (
     <header id="header" className={`${NeutralFace.className}`}>
       <div className="header-container blocco">
-        {/* <a href="/">VANTABLACK</a> */}
         <div id="time">
           <LocalTime />
         </div>
         <div id="menu">
           <a href="/about">ABOUT</a>
-          <a href="#services2">SKILLS</a>
+          <a href="#services">SKILLS</a>
           <a href="#projects">PROJECTS</a>
           <a href="#footer">CONTACT</a>
         </div>
