8d30348de10cc4b8eedfba7512c1f188e31825ef
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2025-01-06 20:01:47 +0100

    fix

--- src/app/graphic-projects/page.js
+++ src/app/graphic-projects/page.js
@@ -17,6 +17,7 @@ export default function GraphicProjectsPage() {
         <img src="/foto/g1.png" alt="Graphic Project 1" />
         <img src="/foto/g2.png" alt="Graphic Project 2" />
         <img src="/foto/g3.jpg" alt="Graphic Project 3" />
+        <img src="/foto/g3.jpg" alt="Graphic Project 3" />
       </div>
     </div>
   );
