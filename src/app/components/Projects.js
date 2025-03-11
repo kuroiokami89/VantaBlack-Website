55a9e06635ee769a0788b28ae46a9b40d9d17392
kuroiokami89 <46483858+kuroiokami89@users.noreply.github.com>
2025-03-11 12:52:42 +0100

    Optimized Projects component, chaged font for span tags

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,50 +1,58 @@
-import { Kusanagi, OrbitronLight } from "./fonts";
+import { Kusanagi, OrbitronLight, Ki } from "./fonts";
+
+// Projects list
+const projects = [
+  {
+    year: "2022",
+    type: "PERSONAL WEBSITE",
+    title: "VANTABLACK WEBSITE 1.0",
+    href: "https://kuroiokami89.github.io/Vantablack-Portfolio-2.0/",
+    className: "item-1",
+  },
+  {
+    year: "2024",
+    type: "FIGMA PROTOTYPE",
+    title: "KRONOS FIGMA PROTHOTYPE",
+    href: "https://www.figma.com/design/geHeoJJUyNJfAzgMKroU15/Kronos--2.0---Ecommerce?node-id=0-1&t=aUKnXH3w3MON96t7-1",
+    className: "item-2",
+  },
+  {
+    year: "2024",
+    type: "E-SHOP",
+    title: "KRONOS ONLINE SHOP",
+    href: "https://vanta-black-kronos.vercel.app/",
+    className: "item-3",
+  },
+  {
+    year: "2025",
+    type: "PORTFOLIO",
+    title: "GRAPHIC WORKS",
+    href: "/graphic-projects",
+    className: "item-4",
+  },
+];
 
 export default function Projects() {
   return (
-    <div id="projects" className={`${OrbitronLight.className}`}>
+    <section id="projects" className={OrbitronLight.className}>
       <div className="section-title">
-        <h2 className={`${Kusanagi.className}`}>PROJECTS</h2>
+        <h2 className={Kusanagi.className}>PROJECTS</h2>
       </div>
       <div className="container">
-        <a
-          href="https://kuroiokami89.github.io/Vantablack-Portfolio-2.0/"
-          className="project-item item-1"
-        >
-          <div className="spans">
-            <span>2022</span>
-            <span>PERSONAL WEBSITE</span>
-          </div>
-          <h3>VANTABLACK WEBSITE 1.0</h3>
-        </a>
-        <a
-          href="https://www.figma.com/design/geHeoJJUyNJfAzgMKroU15/Kronos--2.0---Ecommerce?node-id=0-1&t=aUKnXH3w3MON96t7-1"
-          className="project-item item-2"
-        >
-          <div className="spans">
-            <span>2024</span>
-            <span>FIGMA</span>
-          </div>
-          <h3>KRONOS FIGMA PROTHOTYPE</h3>
-        </a>
-        <a
-          href="https://vanta-black-kronos.vercel.app/"
-          className="project-item item-3"
-        >
-          <div className="spans">
-            <span>2024</span>
-            <span>E-SHOP</span>
-          </div>
-          <h3>KRONOS ONLINE SHOP</h3>
-        </a>
-        <a href="/graphic-projects" className="project-item item-4">
-          <div className="spans">
-            <span>2025</span>
-            <span>PORTFOLIO</span>
-          </div>
-          <h3>GRAPHIC WORKS</h3>
-        </a>
+        {projects.map((project, index) => (
+          <a
+            key={index}
+            href={project.href}
+            className={`project-item ${project.className}`}
+          >
+            <div className={`spans ${Ki.className}`}>
+              <span>{project.year}</span>
+              <span>{project.type}</span>
+            </div>
+            <h3>{project.title}</h3>
+          </a>
+        ))}
       </div>
-    </div>
+    </section>
   );
 }
